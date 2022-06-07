ece5d07b9a0c412f367a7c5a076df2309fc15385
samarth chandrawat <samarthchandrawat@gmail.com>
2022-06-08 01:42:35 +0530

    Time: 10 ms (20.24%), Space: 11.1 MB (75.41%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
-    
-    int helper(vector<int> &nums, int target, int start, int end) {
+    int search(vector<int>& nums, int target) {
         
-        if(start > end) {
-            return -1;
-        }
+        int start = 0;
+        int end = nums.size() - 1;
         
-        int mid = (start + end)/2;
-        
-        if(nums[mid] == target) {
-            return mid;
-        }
-        
-        //mid is before pivot
-        if(nums[start] <= nums[mid]) {
+        while( start <= end ) {
             
-            if(target >= nums[start] && target <= nums[mid]) {
-                
-                return helper(nums, target, start, mid);
-
-            } else {
-                return helper(nums, target, mid + 1, end);
+            int mid = (start + end)/2;
+            
+            if(nums[mid] == target) {
+                return mid;
             }
-        } 
-        
-        else if (nums[mid] <= nums[end]) {
             
-            if(target >= nums[mid] && target <= nums[end]) {
+            //mid lies before pivot
+            if(nums[start] <= nums[mid]) {
                 
-                return helper(nums, target, mid, end);
-            
+                if(target >= nums[start] && target <= nums[mid]) {
+                    
+                    end = mid;
+                } else {
+                   
+                    start = mid + 1;
+                }
             } else {
-                return helper(nums, target, start, mid-1);
+                //mid lies after pivot
+                if(target >= nums[mid] && target <= nums[end]) {
+                    
+                    start = mid;
+                } else {
+                   
+                    end = mid - 1;
+                }
             }
         }
         
         return -1;
-    }
-    
-    
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        return helper(nums, target, start, end);
     }
 };
