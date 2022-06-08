0d29db2fded12120a59fbd6360f9490a762f1ca7
samarth chandrawat <samarthchandrawat@gmail.com>
2022-06-08 22:01:54 +0530

    Time: 14 ms (8.27%), Space: 8.8 MB (89.45%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -4,26 +4,16 @@ public:
         int start = 0;
         int end = nums.size()-1;
         
-        while(start <= end) {
+        while(start < end) {
             
             int mid = (start + end)/2;
             
-            if( (mid+1 <= nums.size()-1) && (mid - 1>=0) &&
-                nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) {
-                return mid;
-            } 
-            else if((mid+1 <= nums.size()-1) && nums[mid+1] > nums[mid]) {
-                start = mid+1;
+            if(nums[mid] > nums[mid+1]) {
+                end = mid;
+            } else {
+                start = start+1;
             }
-            else if((mid - 1>=0) && nums[mid-1] > nums[mid]) {
-                end = mid -1;
-            } 
-            else if(mid ==0) {
-                return 0;
-            } 
-            else if(mid == nums.size()-1) {
-                return nums.size()-1;
-            }
+            
         }
         
         return start;
