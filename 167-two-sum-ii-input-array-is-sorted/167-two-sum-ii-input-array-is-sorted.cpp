66e200f39c1ce99b5d350eb59fd7644e92b7f941
samarth chandrawat <samarthchandrawat@gmail.com>
2022-06-09 22:38:43 +0530

    Time: 25 ms (22.68%), Space: 15.6 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,49 +3,36 @@ public:
     
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        vector<int> res(2,-1);
-        
-        for(int i=0; i<nums.size(); i++) {
+        for(int i=0; i<nums.size()-1; i++) {
             
             int start = i + 1;
             int end = nums.size() - 1;
             
-            
-            while((i+1 < nums.size()) && nums[i] == nums[i+1]) {
-                if(nums[i] + nums[i+1] != target)
+            while(nums[i] == nums[i+1]) {
+                if(nums[i] + nums[i+1] == target)
+                        return {i+1, i+2};
+                else 
                     i++;
-                else {
-                    res[0] = i+1;
-                    res[1] = i+2;
-                    return res;
-                }
             }
-            
-            int temp_target = target - nums[i];
-            
-            if(temp_target < 0) 
-                return res;
             
             while(start <= end) {
                 
                 int mid  = (start + end)/2;
                 
-                if(nums[mid] == temp_target) {
-                    res[0] = i+1;
-                    res[1] = mid+1;
-                    return res;
+                if(nums[mid] == target - nums[i]) {
+                    return {i + 1, mid + 1};
                 }
                 
-                else if (nums[mid] > temp_target) {
+                else if (nums[mid] > target - nums[i]) {
                     end = end - 1;
                 }
                 
                 else {
-                    start = start + 1;
+                    start = mid + 1;
                 }
             }
             
         }
-       return res; 
+       return {-1, -1}; 
     }
 };
