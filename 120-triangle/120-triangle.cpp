1e998b42626507c9a2c2ade23c8a581ce5870a0b
samarth chandrawat <samarthchandrawat@gmail.com>
2022-06-13 23:00:33 +0530

    Time: 8 ms (61.32%), Space: 8.5 MB (98.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -5,7 +5,6 @@ public:
         
         int base_len = triangle[triangle.size()-1].size();
         
-        
         for(int i = base_len-2; i >= 0; i--) {
             
             for(int j=0; j<=i; j++) {
