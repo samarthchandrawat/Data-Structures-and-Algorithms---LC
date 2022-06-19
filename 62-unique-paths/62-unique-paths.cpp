10e66af32ea0dbeb127536d09ee949f1964b6368
samarth chandrawat <samarthchandrawat@gmail.com>
2022-06-19 18:08:59 +0530

    Time: 3 ms (43.18%), Space: 6.5 MB (25.32%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,50 +1,17 @@
 class Solution {
 public:
-    
-    int rec(int m, int n, int i, int j, vector<vector<int>> &ways) {
-        
-        if(i>=m && j>=n) {
-            
-            return 0;
-        }
-        
-        if(i==m-1 && j==n-1) {
-            
-            return 1;
-        }
-        
-        
-        if(ways[i][j+1] == -1) {
-            
-            ways[i][j+1] = rec(m, n, i, j+1, ways);
-        }
-        
-        if(ways[i+1][j] == -1) {
-            
-            ways[i+1][j] = rec(m, n, i+1, j, ways);
-        }
-        
-        ways[i][j] = ways[i+1][j] + ways[i][j+1];
-        
-        return ways[i][j];
-        
-    } 
-    
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> ways(m+1, vector<int>(n+1, -1));
+        vector<vector<int>> ways(m, vector<int>(n, 1));
         
-        for(int i=0; i<=n; i++) {
-            ways[m][i] = 0;
+        for(int i=1; i<m; i++) {
+            
+            for(int j=1; j<n; j++) {
+                
+                ways[i][j] = ways[i-1][j] + ways[i][j-1];
+            }
         }
         
-        for(int i=0; i<=m; i++) {
-            ways[i][n] = 0;
-        }
-        
-        rec(m, n, 0, 0, ways);
-        
-        
-        return (ways[0][0] == -1 ? 1 : ways[0][0]);
+        return ways[m-1][n-1];
     }
 };
