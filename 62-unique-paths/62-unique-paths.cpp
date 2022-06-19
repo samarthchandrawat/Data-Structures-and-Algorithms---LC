6011303c0fd502e38fb2dfcdafec4427d9891f8a
samarth chandrawat <samarthchandrawat@gmail.com>
2022-06-19 18:25:00 +0530

    Time: 2 ms (48.43%), Space: 6.2 MB (56.29%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,17 +3,15 @@ public:
     int uniquePaths(int m, int n) {
         
         vector<int> curr_row(n, 1);
-        vector<int> prev_row(n, 1);
         
         for(int i=1; i<m; i++) {
             
             for(int j=1; j<n; j++) {
                 
-                curr_row[j] = prev_row[j] + curr_row[j-1];
+                curr_row[j] = curr_row[j] + curr_row[j-1];
             }
-            swap(prev_row, curr_row);
         }
         
-        return prev_row[n-1];
+        return curr_row[n-1];
     }
 };
