0aecdd7bea313fe7d4d885f8f1953ef8f8acbcdf
samarth chandrawat <samarthchandrawat@gmail.com>
2023-05-28 15:51:59 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,53 +8,26 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    
-    int helper(int w, int n, int wt[], int val[], vector<vector<int>> &dp) {
-        
-        if(w==0 || n==0)
-            return 0;
-        
-        int inc = 0;
-        int exc = 0;
-        
-        if(dp[w][n] != -1) {
-            
-            return dp[w][n];
-        }
-        
-        if(w-wt[n-1] >= 0) {
-            
-            inc = val[n-1] + helper(w-wt[n-1], n-1, wt, val, dp);    
-        }
-        
-        exc = helper(w, n-1, wt, val, dp);
-        
-        return dp[w][n] = max(exc, inc);
-    }
-    
-    int knapSack(int w, int wt[], int val[], int n) 
+    int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-        vector<int> dp(w+1, 0);
-        for(int i=1; i<=n; i++) {
-            
-            for(int j=w; j>=1; j--) {
-                
-                int inc = 0;
-                int exc = 0;
-                
-                if(j - wt[i-1] >= 0) {
-                    
-                    inc = dp[j - wt[i-1]] + val[i-1];    
-                }
-                
-                exc = dp[j];
-                
-                dp[j] = max(inc, exc);
-            }
-        }
-        
-        return dp[w];
+       
+       vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+       
+       for(int i=1; i<=n; i++) {
+           
+           for(int j=1; j<=W; j++) {
+               
+               if(wt[i-1] <= j) {
+                   
+                   dp[i][j] = max(dp[i][j], val[i-1] + dp[i-1][j-wt[i-1]]);
+               }
+               
+               dp[i][j] = max(dp[i][j], dp[i-1][j]);
+           }
+       }
+       
+       return dp[n][W];
     }
 };
 
